ca0f3830b0c38023e71cf610cf0693d7cc1f0cda
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-01-06 14:01:51 +0100

    initial Typescript in game-context

--- src/store/game-context.tsx
+++ src/store/game-context.tsx
@@ -1,38 +1,62 @@
-/* eslint-disable no-mixed-operators */
-import { useState, createContext } from "react";
+import { createContext } from "react";
 
-interface GameContextInterface {
-  endGame: Boolean;
-  setEndGame: () => void;
-  player: String;
-  setPlayer: () => void;
-}
+import { DUMMY_CARDS } from "../utilities/cards";
 
-const GameContext = createContext<GameContextInterface>({
-  endGame: false,
-  setEndGame: () => {},
-  player: "igor",
-  setPlayer: () => {},
-});
-
-export const GameContextProvider: React.FC<{ children: React.ReactNode }> = (
-  props
-) => {
-  const [endGame, setEndGame] = useState(false);
-  const [player, setPlayer] = useState("igor");
-
-  return (
-    <GameContext.Provider
-      value={{
-        endGame: endGame,
-        setEndGame
-        player: player,
-        setPlayer,
-      }}
-    >
-      {props.children}
-    </GameContext.Provider>
-  );
+type CardObj = {
+  color: string;
+  id: number;
+  flipped: boolean;
 };
 
-export default GameContext;
+type GameContextObj = {
+  cards: CardObj[];
+};
+
+const GameContext = createContext<GameContextObj>({
+  cards: DUMMY_CARDS,
+});
+
+export const GameContextProvider: React.FC = (props) => {
+  return <GameContext.Provider value={"siema"}></GameContext.Provider>;
+};
+// import { useState, createContext } from "react";
+// import { DUMMY_CARDS } from "../utilities/cards";
+
+// interface GameContextInterface {
+//   cards:any;
+//   endGame: Boolean;
+//   setEndGame: () => void;
+//   player: String;
+//   setPlayer: () => void;
+// }
+
+// const GameContext = createContext<GameContextInterface>({
+//   cards: DUMMY_CARDS,
+//   endGame: false,
+//   setEndGame: () => {},
+//   player: "igor",
+//   setPlayer: () => {},
+// });
+
+// export const GameContextProvider: React.FC<{ children: React.ReactNode }> = (
+//   props
+// ) => {
+//   const [endGame, setEndGame] = useState(false);
+//   const [player, setPlayer] = useState("igor");
+
+//   return (
+//     <GameContext.Provider
+//       value={{
+//         cards:cards
+//         endGame: endGame,
+//         setEndGame
+//         player: player,
+//         setPlayer,
+//       }}
+//     >
+//       {props.children}
+//     </GameContext.Provider>
+//   );
+// };
+
+// export default GameContext;
